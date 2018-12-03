9074404f276e0bf6f375031f0fbb11cc3fc9fbee
Your Name <lulu.zheng@tufts.edu>
2018-12-02 21:31:21 -0500

    fix

---
+++ math.py
@@ -0,0 +1,16 @@
+#
+#  math.py
+#
+
+def add(n1, n2):
+    return n1 + n2
+
+def minus(n1, n2):
+    return n1 - n2
+
+def multiple(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
